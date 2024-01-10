b33e4c00c6b9d6becf1bb74363e2f342cd61c002
lavishiratzky <lavi.shiratzky@gmail.com>
2024-01-10 11:29:52 +0200

    End of Section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,11 +1,28 @@
 
+import { useEffect, useState } from 'react';
 import './App.css'
+import axios from 'axios';
+import { Header, List, ListItem } from 'semantic-ui-react';
 
 function App() {
-  
-
+  const [activities , setActivities ]=useState([]);
+useEffect(()=>{
+  axios.get('http://localhost:5000/activities')
+            .then(response=>{setActivities(response.data)})
+},[])
   return (
-    <h1>LAVIS APP</h1>
+    <div>
+        <Header as ='h2' icon='american sign language interpreting' content='Reactivities'/>
+         <List>
+            {activities.map((activity: any)=>(
+              <List.Item key={activity.id}>
+                {activity.title}
+                
+              </List.Item>
+            ))}
+         </List>
+    </div>
+   
   )
 }
 
